f3367f8050b613e7e2e1718b426831c88e87a6fd
Carling <carlingkirk@gmail.com>
2020-07-26 08:05:36 -0400

    Add migration for new fields on Location

--- src/Emergence.Data/Migrations/20200726115004_Inc_Photos.Designer.cs
+++ src/Emergence.Data/Migrations/20200726115004_Inc_Photos.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Emergence.Data.Repository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Emergence.Data.Migrations
 {
     [DbContext(typeof(EmergenceDbContext))]
-    partial class EmergenceDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200726115004_Inc_Photos")]
+    partial class Inc_Photos
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
