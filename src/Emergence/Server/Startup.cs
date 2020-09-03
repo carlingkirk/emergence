5c806d396c036c98f9563a36ff61327bc07e59dd
Carling <carlingkirk@gmail.com>
2020-09-03 08:16:58 -0400

    Add a debug mode option in config so we can try and figure out this auth issue

--- src/Emergence/Server/Startup.cs
+++ src/Emergence/Server/Startup.cs
@@ -129,28 +129,31 @@ namespace Emergence.Server
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            var debugMode = Configuration["App:DebugMode"] == "true";
+
+            if (env.IsDevelopment() || debugMode)
             {
                 app.UseDeveloperExceptionPage();
                 app.UseDatabaseErrorPage();
                 app.UseWebAssemblyDebugging();
+
+                // Enable middleware to serve generated Swagger as a JSON endpoint.
+                app.UseSwagger();
+
+                // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+                // specifying the Swagger JSON endpoint.
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Emergence V1");
+                });
+
             }
             else
             {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                //app.UseHsts();
+                app.UseHsts();
             }
-
-            // Enable middleware to serve generated Swagger as a JSON endpoint.
-            app.UseSwagger();
-
-            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
-            // specifying the Swagger JSON endpoint.
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Emergence V1");
-            });
 
             app.UseHttpsRedirection();
             app.UseBlazorFrameworkFiles();
