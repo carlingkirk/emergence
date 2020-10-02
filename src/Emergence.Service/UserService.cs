3d57f0b15fb17bd961ee71ecba9a4a59bf504896
Carling <carlingkirk@gmail.com>
2020-10-02 06:17:11 -0400

    Get photo from PhotoService so we can get blob path

--- src/Emergence.Service/UserService.cs
+++ src/Emergence.Service/UserService.cs
@@ -10,29 +10,54 @@ namespace Emergence.Service
     public class UserService : IUserService
     {
         private readonly IRepository<User> _userRepository;
+        private readonly IPhotoService _photoService;
 
-        public UserService(IRepository<User> userRepository)
+        public UserService(IRepository<User> userRepository, IPhotoService photoService)
         {
             _userRepository = userRepository;
+            _photoService = photoService;
         }
 
         public async Task<Data.Shared.Models.User> GetUserAsync(int id)
         {
-            var user = await _userRepository.GetAsync(u => u.Id == id);
-            return user.AsModel();
+            var userResult = await _userRepository.GetWithIncludesAsync(u => u.Id == id);
+            var userModel = userResult.AsModel();
+
+            if (userResult.PhotoId.HasValue)
+            {
+                var photo = await _photoService.GetPhotoAsync(userResult.PhotoId.Value);
+                userModel.Photo = photo;
+            }
+
+            return userModel;
         }
 
         public async Task<Data.Shared.Models.User> GetUserAsync(string userId)
         {
             var userGuid = new Guid(userId);
-            var user = await _userRepository.GetAsync(u => u.UserId == userGuid);
-            return user.AsModel();
+            var userResult = await _userRepository.GetAsync(u => u.UserId == userGuid);
+            var userModel = userResult.AsModel();
+
+            if (userResult.PhotoId.HasValue)
+            {
+                var photo = await _photoService.GetPhotoAsync(userResult.PhotoId.Value);
+                userModel.Photo = photo;
+            }
+
+            return userModel;
         }
 
         public async Task<Data.Shared.Models.User> UpdateUserAsync(Data.Shared.Models.User user)
         {
             var userResult = await _userRepository.UpdateAsync(user.AsStore());
-            return userResult.AsModel();
+            var userModel = userResult.AsModel();
+            if (userResult.PhotoId.HasValue)
+            {
+                var photo = await _photoService.GetPhotoAsync(userResult.PhotoId.Value);
+                userModel.Photo = photo;
+            }
+
+            return userModel;
         }
     }
 }
