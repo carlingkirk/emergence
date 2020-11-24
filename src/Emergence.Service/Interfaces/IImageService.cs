4bbcd99b3eec448e01837e0a17e16d246d3eceae
Carling <carlingkirk@gmail.com>
2020-11-24 05:06:24 -0500

    Remove unused interface

--- src/Emergence.Service/Interfaces/IImageService.cs
+++
@@ -1,9 +0,0 @@
-using static System.Net.Mime.MediaTypeNames;
-
-namespace Emergence.Service.Interfaces
-{
-    public interface IImageService
-    {
-        Image ResizePhoto(Image image, int maxDimension);
-    }
-}
