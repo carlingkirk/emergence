4c59ec0f59517ef656f0706a5455a2c8cd649e02
Carling Kirk <carlingkirk@gmail.com>
2021-02-02 09:26:51 -0500

    Workaround for System.Text.Json bug

--- src/Emergence.Data.Shared/Search/Filter.cs
+++ src/Emergence.Data.Shared/Search/Filter.cs
@@ -12,61 +12,22 @@ namespace Emergence.Data.Shared.Search
     public class Filter<TValue> : Filter
     {
         public TValue Value { get; set; }
-        public virtual bool IsFiltered() => Value switch
-        {
-            string stringValue => !string.IsNullOrEmpty(stringValue),
-            int intValue => intValue != 0,
-            _ => false,
-        };
     }
 
     public class SelectFilter<TValue> : Filter<TValue>
     {
-        public IDictionary<TValue, long?> FacetValues { get; set; }
-        public override bool IsFiltered() => FacetValues != null && FacetValues switch
-        {
-            string stringValue => !string.IsNullOrEmpty(stringValue),
-            int intValue => intValue != 0,
-            double doubleValue => doubleValue != 0,
-            _ => false
-        };
+        public Dictionary<TValue, long?> FacetValues { get; set; }
     }
 
     public class SelectRangeFilter<TValue> : RangeFilter<TValue>
     {
-        public IDictionary<TValue, long?> MinFacetValues { get; set; }
-        public IDictionary<TValue, long?> MaxFacetValues { get; set; }
-        public override bool IsFiltered() => MinFacetValues != null && MinFacetValues switch
-        {
-            string stringValue => !string.IsNullOrEmpty(stringValue),
-            int intValue => intValue != 0,
-            double doubleValue => doubleValue != 0,
-            _ => false
-        };
+        public Dictionary<TValue, long?> MinFacetValues { get; set; }
+        public Dictionary<TValue, long?> MaxFacetValues { get; set; }
     }
 
     public class RangeFilter<TValue> : SelectFilter<TValue>
     {
         public TValue MinimumValue { get; set; }
         public TValue MaximumValue { get; set; }
-
-        public override bool IsFiltered()
-        {
-            var minValue = MinimumValue switch
-            {
-                string stringValue => !string.IsNullOrEmpty(stringValue),
-                int intValue => intValue != 0,
-                _ => false,
-            };
-
-            var maxValue = MaximumValue switch
-            {
-                string stringValue => !string.IsNullOrEmpty(stringValue),
-                int intValue => intValue != 0,
-                _ => false,
-            };
-
-            return minValue || maxValue;
-        }
     }
 }
