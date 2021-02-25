1ebea8f5a14feab63a3c48a0f95fa548e75ae62f
Carling Kirk <carlingkirk@gmail.com>
2021-02-25 16:19:38 -0500

    Migrate and fix bugs

---
+++ src/Emergence.Data/Migrations/20210225204523_ConvservationStatus.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Emergence.Data.Migrations
+{
+    public partial class ConvservationStatus : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ConservationStatus",
+                table: "PlantLocations",
+                type: "nvarchar(36)",
+                maxLength: 36,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ConservationStatus",
+                table: "PlantLocations");
+        }
+    }
+}
