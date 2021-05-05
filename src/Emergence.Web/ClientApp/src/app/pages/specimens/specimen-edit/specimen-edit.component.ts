2d2e6482112b05ec485f8a2f87531339d93cf662
Carling Kirk <carlingkirk@gmail.com>
2021-05-05 07:21:14 -0400

    Bare minimum edit specimen page

--- src/Emergence.Web/ClientApp/src/app/pages/specimens/specimen-edit/specimen-edit.component.ts
+++ src/Emergence.Web/ClientApp/src/app/pages/specimens/specimen-edit/specimen-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SpecimenService } from 'src/app/service/specimen-service';
+import { InventoryItemStatus, ItemType, SpecimenStage, Visibility } from 'src/app/shared/models/enums';
 import { Specimen } from 'src/app/shared/models/specimen';
 
 @Component({
@@ -9,10 +12,46 @@ import { Specimen } from 'src/app/shared/models/specimen';
 export class SpecimenEditComponent implements OnInit {
   @Input()
   public specimen: Specimen;
-  
-  constructor() { }
+  @Input()
+  public id: number;
+  public inventoryItemStatuses: InventoryItemStatus[];
+  public visibilities: Visibility[];
+  public specimenStages: SpecimenStage[];
+  public itemTypes: ItemType[];
+  public modal: boolean;
+  private inventoryItemStatusEnum = InventoryItemStatus;
+
+  constructor(private readonly specimenService: SpecimenService,
+    private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+
+    this.inventoryItemStatuses = Object.keys(InventoryItemStatus).filter(key => !isNaN(Number(key))).map(key => InventoryItemStatus[key]);
+    this.visibilities = Object.keys(Visibility).filter(key => !isNaN(Number(key))).map(key => Visibility[key]);
+    this.specimenStages = Object.keys(SpecimenStage).filter(key => !isNaN(Number(key))).map(key => SpecimenStage[key]);
+    this.itemTypes = Object.keys(ItemType).filter(key => !isNaN(Number(key))).map((key) => ItemType[key]);
+
+    if (!this.specimen) {
+      this.specimenService.getSpecimen(this.id).subscribe((specimen) => {
+        this.specimen = specimen;
+      });
+    }
   }
 
+  public saveSpecimen(): void {
+
+  }
+
+  public populateInventoryItemName(): void {
+
+  }
+
+  public cancel(): void {
+
+  }
+
+  public closeModal(): void {
+    
+  }
 }
--- src/Emergence.Web/ClientApp/src/app/shared/models/enums.ts
+++ src/Emergence.Web/ClientApp/src/app/shared/models/enums.ts
@@ -1,5 +1,4 @@
-export enum SpecimenStage
-{
+export enum SpecimenStage {
     'Unknown',
     'Seed',
     'Ordered',
@@ -11,4 +10,25 @@ export enum SpecimenStage
     'Diseased',
     'Deceased',
     'Dormant'
+}
+
+export enum InventoryItemStatus {
+    'Available',
+    'Wishlist',
+    'Ordered',
+    'In Use'
+}
+
+export enum Visibility {
+    'Inherit from profile',
+    'Public',
+    'My Contacts',
+    'Private'
+}
+
+export enum ItemType {
+    Specimen,
+    Supply,
+    Container,
+    Tool
 }
