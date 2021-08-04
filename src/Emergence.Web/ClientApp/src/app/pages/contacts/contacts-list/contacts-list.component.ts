57e7d30017451f1b50b2dd05bc4d248891d9b088
Carling Kirk <carlingkirk@gmail.com>
2021-08-04 07:26:06 -0400

    Contacts list

--- src/Emergence.Web/ClientApp/src/app/pages/contacts/contacts-list/contacts-list.component.ts
+++ src/Emergence.Web/ClientApp/src/app/pages/contacts/contacts-list/contacts-list.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { ContactsService } from 'src/app/service/contacts-service';
+import { Column } from 'src/app/shared/components/sortable-headers/sortable-headers.component';
+import { IListable, Listable } from 'src/app/shared/interface/list';
+import { UserContact } from 'src/app/shared/models/user-contacts';
 
 @Component({
   selector: 'app-contacts-list',
   templateUrl: './contacts-list.component.html',
   styleUrls: ['./contacts-list.component.css']
 })
-export class ContactsListComponent implements OnInit {
+export class ContactsListComponent extends Listable implements OnInit, IListable {
 
-  constructor() { }
+  public columns: Column[] = [
+    { name: 'Display Name', value: 'DisplayName'},
+    { name: 'Date Accepted', value: 'DateAccepted'}
+  ];
+  public contacts: UserContact[];
 
-  ngOnInit(): void {
+  constructor(
+    private readonly contactsService: ContactsService
+  ) { 
+    super();
   }
 
+  ngOnInit(): void {
+      this.searchRequest = {
+        take: 12,
+        skip: 0,
+        useNGrams: false,
+        sortDirection: 0
+      };
+      this.loadContacts();
+    }
+
+  loadContacts() {
+    this.contactsService.findContactsAsync(this.searchRequest).subscribe(
+      (searchResult) => {
+        this.searchResult = searchResult;
+        this.contacts = searchResult.results;
+        this.totalCount = searchResult.count;
+        this.searchRequest.filters = searchResult.filters;
+      }
+    );
+  }
+  
+  public search(): void {
+    this.loadContacts();
+  }
 }
